1c9833c6475b62bbf66777e5257b28aad5ec03ee
Mocanita Robert <mocanitarobert@yahoo.com>
2022-11-30 10:44:16 +0200

    added 2fa srv-cli pair

---
+++ cli2fa.c
@@ -0,0 +1,10 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <string.h>
+
